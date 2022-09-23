022ab5e3db2edaf24633ff7b09bddf9de60dad21
hegarcia <hector.garcia.cabrera@cern.ch>
2022-09-23 11:20:29 +0200

    First Public Version

--- eventbuilding/include/ECal_EventScheme.h
+++ eventbuilding/include/ECal_EventScheme.h
@@ -1,7 +1,7 @@
 #ifndef __ECAL_EVENTSCHEME__
 #define __ECAL_EVENTSCHEME__
 
-//#include <vector>
+#include <vector>
 #include <map>
 
 #define SLBDEPTH 15
@@ -10,9 +10,17 @@
 #define NCHANNELS 64
 #define NSCA 15
 
-struct RawECALEvent {
+#define ARRAYSIZE 9999
 
-  int event;
+struct Branches {
+
+  std::map<std::string, void*> branchesMap = {};
+  
+};
+
+
+struct RawECALEvent : Branches {
+
   int acqNumber; 
   int n_slboards;
 
@@ -35,13 +43,16 @@ struct RawECALEvent {
   int badbcid[SLBDEPTH][NCHIP][MEMDEPTH];
   int nhits[SLBDEPTH][NCHIP][MEMDEPTH];
 
-  int lowGain[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
-  int highGain[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
-  int gain_hit_low[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
-  int gain_hit_high[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
-
-  std::map<std::string, void*> branchesMap = {
-						{"event",&event},
+  int adc_low[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
+  int adc_high[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
+  int autogainbit_low[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
+  int autogainbit_high[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
+  int hitbit_low[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
+  int hitbit_high[SLBDEPTH][NCHIP][MEMDEPTH][NCHANNELS];
+  
+  RawECALEvent() {
+    
+    branchesMap = {
 						{"acqNumber",&acqNumber},
 						{"n_slboards",&n_slboards},
 						{"slot",slot},
@@ -59,13 +70,88 @@ struct RawECALEvent {
 						{"corrected_bcid",corrected_bcid},
 						{"badbcid",badbcid},
 						{"nhits",nhits},
-						{"charge_lowGain",lowGain},
-						{"charge_hiGain",highGain},
-						{"gain_hit_low",gain_hit_low},
-						{"gain_hit_high",gain_hit_high}
-						
-  };
+						{"adc_low",adc_low},
+						{"adc_high",adc_high},
+						{"autogainbit_low",autogainbit_low},
+						{"autogainbit_high",autogainbit_high},
+						{"hitbit_low",hitbit_low},
+						{"hitbit_high",hitbit_high}
+    };
+  }
   
+};
+
+
+struct BuiltROOTEvent : Branches {
+
+  int event; // Event counter within a cycle
+  int spill; // DAQ cycles in which data was written
+  int cycle; // Readout number 
+  int bcid;
+  int bcid_first_sca_full;
+  int bcid_merge_end;
+  int id_run; // Run number
+  int id_dat; // Raw data file from which this event was read 
+  int nhit_slab; // Number of slabs with hits
+  int nhit_chip; // Number of chips wich hits
+  int nhit_chan; // Number of channels with hits
+  int nhit_len; // Array sizes. Should be equal to nhit_chan unless zero supression is removed
+
+  float sum_energy;
+  float sum_energy_lg;
+
+  int hit_slab[ARRAYSIZE];
+  int hit_chip[ARRAYSIZE];
+  int hit_chan[ARRAYSIZE];
+  int hit_sca[ARRAYSIZE];
+  int hit_adc_high[ARRAYSIZE];
+  int hit_adc_low[ARRAYSIZE];
+  int hit_n_scas_filled[ARRAYSIZE];
+  int hit_isHit[ARRAYSIZE];
+  int hit_isMasked[ARRAYSIZE];
+  int hit_isCommissioned[ARRAYSIZE];
+
+  float hit_x[ARRAYSIZE];
+  float hit_y[ARRAYSIZE];
+  float hit_z[ARRAYSIZE];
+  float hit_energy[ARRAYSIZE];
+  float hit_energy_lg[ARRAYSIZE];
+
+  BuiltROOTEvent() {
+  
+    branchesMap = {
+					      {"event",&event},
+					      {"spill",&spill},
+					      {"cycle",&cycle},
+					      {"bcid",&bcid},
+					      {"bcid_first_sca_full",&bcid_first_sca_full},
+					      {"bcid_merge_end",&bcid_merge_end},
+					      {"id_run",&id_run},
+					      {"id_dat",&id_dat},
+					      {"nhit_slab",&nhit_slab},
+					      {"nhit_chip",&nhit_chip},
+					      {"nhit_chan",&nhit_chan},
+					      {"nhit_len",&nhit_len},
+					      {"sum_energy",&sum_energy},
+					      {"sum_energy_lg",&sum_energy_lg},
+					      {"hit_slab",hit_slab},
+					      {"hit_chip",hit_chip},
+					      {"hit_chan",hit_chan},
+					      {"hit_sca",hit_sca},
+					      {"hit_adc_high",hit_adc_high},
+					      {"hit_adc_low",hit_adc_low},
+					      {"hit_n_scas_filled",hit_n_scas_filled},
+					      {"hit_isHit",hit_isHit},
+					      {"hit_isMasked",hit_isMasked},
+					      {"hit_isCommissioned",hit_isCommissioned},
+					      {"hit_x",hit_x},
+					      {"hit_y",hit_y},
+					      {"hit_z",hit_z},
+					      {"hit_energy",hit_energy},
+					      {"hit_energy_lg",hit_energy_lg}					      
+    };
+  }
+    
 };
 
 #endif
---
+++ eventbuilding/script/initWorkEnv.sh
@@ -0,0 +1,4 @@
+unset MARLIN_DLL
+softdir=/home/hecgc/Physics/ILCSoft/v02-02-02
+source ${softdir}/init_ilcsoft.sh
+
